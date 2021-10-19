be8400d59e8a6d94c91ddae0c6ffcd227dd57155
Swasthik-k-s <swasthik.ks22@gmail.com>
2021-10-19 10:59:42 +0530

    Initial Commit

---
+++ AuthenticationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthenticationPractice
+//
+//  Created by Swasthik K S on 19/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
