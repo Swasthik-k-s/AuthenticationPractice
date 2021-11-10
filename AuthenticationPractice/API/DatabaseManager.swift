7fbbf5fed17d1d9b025e0a63a735606bb3d972af
Swasthik <62685591+Swasthik-k-s@users.noreply.github.com>
2021-11-10 21:48:08 +0530

    Add files via upload

---
+++ AuthenticationPractice/API/DatabaseManager.swift
@@ -0,0 +1,33 @@
+//
+//  DatabaseManager.swift
+//  AuthenticationPractice
+//
+//  Created by Swasthik K S on 01/11/21.
+//
+
+import Foundation
+import UIKit
+
+struct DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    func addNote(note: [String: Any]) {
+        NetworkManager.shared.addNote(note: note)
+//        PersistentManager.shared.addNote(note: realmNote)
+    }
+    
+    func updateNote(note: NoteItem) {
+        NetworkManager.shared.updateNote(note: note)
+//        PersistentManager.shared.updateNote(note: realmNote, title: title, description: description)
+    }
+    
+    func deleteNote(note: NoteItem) {
+        NetworkManager.shared.deleteNote(note: note)
+//        if realmNote.title != "" {
+////            PersistentManager.shared.deleteNote(note: realmNote)
+//        }
+        
+    }
+    
+}
+
